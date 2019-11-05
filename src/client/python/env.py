52b4ca1e748519d5b9222f740b8b33e91e192204
Bob Jacobs <bob.jacobs@outlook.com>
2019-11-05 09:17:28 -0500

    updating the env.py script to incude operating system version

--- src/client/python/env.py
+++ src/client/python/env.py
@@ -1,9 +1,11 @@
 import os
 import pprint
+import platform
 
 def main():
     env_var = os.environ
 
+    print('The operating system version is ' + platform.platform())
     print("User Env Variables:")
     pprint.pprint(dict(env_var), width = 1) 
 
