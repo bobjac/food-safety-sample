984faa4f5dba1d105745c9475af1b3ade085e04f
bobjac <bob.jacobs@outlook.com>
2019-11-07 00:19:48 -0500

    setting up telemetry client

--- src/client/python/app.py
+++ src/client/python/app.py
@@ -58,23 +58,140 @@ def is_correct_connection_string():
         return False
 
 def send_temp_and_humidity(sensor, pin):
+
+    try:
+        client = iothub_client_init()
+        telemetry.send_telemetry_data(parse_iot_hub_name(), EVENT_SUCCESS, "IoT hub connection is established")
+        
+        if client.protocol == IoTHubTransportProvider.MQTT:
+            print ( "IoTHubClient is reporting state" )
+            reported_state = "{\"newState\":\"standBy\"}"
+            client.send_reported_state(reported_state, len(reported_state), send_reported_state_callback, SEND_REPORTED_STATE_CONTEXT)
     
-    # Try to grab a sensor reading.  Use the read_retry method which will retry up
-    # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+        # Try to grab a sensor reading.  Use the read_retry method which will retry up
+        # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
-    # Un-comment the line below to convert the temperature to Fahrenheit.
-    temperature = temperature * 9/5.0 + 32
+        # Un-comment the line below to convert the temperature to Fahrenheit.
+        temperature = temperature * 9/5.0 + 32
 
-    # Note that sometimes you won't get a reading and
-    # the results will be null (because Linux can't
-    # guarantee the timing of calls to read the sensor).
-    # If this happens try again!
-    if humidity is not None and temperature is not None:
-        print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-    else:
-        print('Failed to get reading. Try again!')
-        sys.exit(1)
+        # Note that sometimes you won't get a reading and
+        # the results will be null (because Linux can't
+        # guarantee the timing of calls to read the sensor).
+        # If this happens try again!
+        if humidity is not None and temperature is not None:
+            print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+        else:
+            print('Failed to get reading. Try again!')
+            sys.exit(1)
+    
+    except IoTHubError as iothub_error:
+        print ( "Unexpected error %s from IoTHub" % iothub_error )
+        telemetry.send_telemetry_data(parse_iot_hub_name(), EVENT_FAILED, "Unexpected error %s from IoTHub" % iothub_error)
+        return
+    except KeyboardInterrupt:
+        print ( "IoTHubClient sample stopped" )
+
+def receive_message_callback(message, counter):
+    global RECEIVE_CALLBACKS
+    message_buffer = message.get_bytearray()
+    size = len(message_buffer)
+    print ( "Received Message [%d]:" % counter )
+    print ( "    Data: <<<%s>>> & Size=%d" % (message_buffer[:size].decode("utf-8"), size) )
+    map_properties = message.properties()
+    key_value_pair = map_properties.get_internals()
+    print ( "    Properties: %s" % key_value_pair )
+    counter += 1
+    RECEIVE_CALLBACKS += 1
+    print ( "    Total calls received: %d" % RECEIVE_CALLBACKS )
+    return IoTHubMessageDispositionResult.ACCEPTED
+
+
+def send_confirmation_callback(message, result, user_context):
+    global SEND_CALLBACKS
+    print ( "Confirmation[%d] received for message with result = %s" % (user_context, result) )
+    map_properties = message.properties()
+    print ( "    message_id: %s" % message.message_id )
+    print ( "    correlation_id: %s" % message.correlation_id )
+    key_value_pair = map_properties.get_internals()
+    print ( "    Properties: %s" % key_value_pair )
+    SEND_CALLBACKS += 1
+    print ( "    Total calls confirmed: %d" % SEND_CALLBACKS )
+    led_blink()
+
+
+def device_twin_callback(update_state, payload, user_context):
+    global TWIN_CALLBACKS
+    print ( "\nTwin callback called with:\nupdateStatus = %s\npayload = %s\ncontext = %s" % (update_state, payload, user_context) )
+    TWIN_CALLBACKS += 1
+    print ( "Total calls confirmed: %d\n" % TWIN_CALLBACKS )
+
+
+def send_reported_state_callback(status_code, user_context):
+    global SEND_REPORTED_STATE_CALLBACKS
+    print ( "Confirmation for reported state received with:\nstatus_code = [%d]\ncontext = %s" % (status_code, user_context) )
+    SEND_REPORTED_STATE_CALLBACKS += 1
+    print ( "    Total calls confirmed: %d" % SEND_REPORTED_STATE_CALLBACKS )
+
+
+def device_method_callback(method_name, payload, user_context):
+    global METHOD_CALLBACKS,MESSAGE_SWITCH
+    print ( "\nMethod callback called with:\nmethodName = %s\npayload = %s\ncontext = %s" % (method_name, payload, user_context) )
+    METHOD_CALLBACKS += 1
+    print ( "Total calls confirmed: %d\n" % METHOD_CALLBACKS )
+    device_method_return_value = DeviceMethodReturnValue()
+    device_method_return_value.response = "{ \"Response\": \"This is the response from the device\" }"
+    device_method_return_value.status = 200
+    if method_name == "start":
+        MESSAGE_SWITCH = True
+        print ( "Start sending message\n" )
+        device_method_return_value.response = "{ \"Response\": \"Successfully started\" }"
+        return device_method_return_value
+    if method_name == "stop":
+        MESSAGE_SWITCH = False
+        print ( "Stop sending message\n" )
+        device_method_return_value.response = "{ \"Response\": \"Successfully stopped\" }"
+        return device_method_return_value
+    return device_method_return_value
+
+
+def blob_upload_conf_callback(result, user_context):
+    global BLOB_CALLBACKS
+    print ( "Blob upload confirmation[%d] received for message with result = %s" % (user_context, result) )
+    BLOB_CALLBACKS += 1
+    print ( "    Total calls confirmed: %d" % BLOB_CALLBACKS )
+
+def iothub_client_init():
+    # prepare iothub client
+    client = IoTHubClient(CONNECTION_STRING, PROTOCOL)
+    client.set_option("product_info", "HappyPath_RaspberryPi-Python")
+    if client.protocol == IoTHubTransportProvider.HTTP:
+        client.set_option("timeout", TIMEOUT)
+        client.set_option("MinimumPollingTime", MINIMUM_POLLING_TIME)
+    # set the time until a message times out
+    client.set_option("messageTimeout", MESSAGE_TIMEOUT)
+    # to enable MQTT logging set to 1
+    if client.protocol == IoTHubTransportProvider.MQTT:
+        client.set_option("logtrace", 0)
+    client.set_message_callback(
+        receive_message_callback, RECEIVE_CONTEXT)
+    if client.protocol == IoTHubTransportProvider.MQTT or client.protocol == IoTHubTransportProvider.MQTT_WS:
+        client.set_device_twin_callback(
+            device_twin_callback, TWIN_CONTEXT)
+        client.set_device_method_callback(
+            device_method_callback, METHOD_CONTEXT)
+    return client
+
+def print_last_message_time(client):
+    try:
+        last_message = client.get_last_message_receive_time()
+        print ( "Last Message: %s" % time.asctime(time.localtime(last_message)) )
+        print ( "Actual time : %s" % time.asctime() )
+    except IoTHubClientError as iothub_client_error:
+        if iothub_client_error.args[0].result == IoTHubClientResult.INDEFINITE_TIME:
+            print ( "No message received" )
+        else:
+            print ( iothub_client_error )
 
 def get_connection_string():
     connection_string = ""
@@ -90,6 +207,20 @@ def get_connection_string():
 
     print('The connection string is ' + connection_string)
     return connection_string
+
+def led_blink():
+    GPIO.output(config.GPIO_PIN_ADDRESS, GPIO.HIGH)
+    time.sleep(config.BLINK_TIMESPAN / 1000.0)
+    GPIO.output(config.GPIO_PIN_ADDRESS, GPIO.LOW)
+
+def usage():
+    print ( "Usage: iothub_client_sample.py -p <protocol> -c <connectionstring>" )
+    print ( "    protocol        : <amqp, amqp_ws, http, mqtt, mqtt_ws>" )
+    print ( "    connectionstring: <HostName=<host_name>;DeviceId=<device_id>;SharedAccessKey=<device_key>>" )
+
+def parse_iot_hub_name():
+    m = re.search("HostName=(.*?)\.", CONNECTION_STRING)
+    return m.group(1)
 
 CONNECTION_STRING = get_connection_string()
 
