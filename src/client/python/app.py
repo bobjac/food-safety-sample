02be67e715d29933c25dc484d358179fd32a8bf0
bobjac <bob.jacobs@outlook.com>
2019-11-07 00:42:17 -0500

    get message timespan from yaml config

--- src/client/python/app.py
+++ src/client/python/app.py
@@ -43,6 +43,8 @@ METHOD_CALLBACKS = 0
 EVENT_SUCCESS = "success"
 EVENT_FAILED = "failed"
 
+MESSAGE_TIMESPAN = get_message_timespan()
+
 # chose HTTP, AMQP or MQTT as transport protocol
 PROTOCOL = IoTHubTransportProvider.MQTT
 
@@ -233,6 +235,15 @@ def get_connection_string():
 
     print('The connection string is ' + connection_string)
     return connection_string
+
+def get_message_timespan():
+    timespan = 1000
+    with open("config.yml", 'r') as ymlfile:
+        cfg = yaml.load(ymlfile, Loader=yaml.BaseLoader)
+    
+    if 'other' in cfg:
+        timespan = cfg['other']['messagetimespan']
+    return timespan
 
 #def led_blink():
 #    GPIO.output(config.GPIO_PIN_ADDRESS, GPIO.HIGH)
