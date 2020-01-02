e267c9c763d5858ff3a768b9882ca3a8752b8597
Bob Jacobs <bob.jacobs@outlook.com>
2020-01-02 14:16:51 -0500

    updating injest service

--- src/client/simulator/dotnet/simulated-device/simulated-device/Program.cs
+++ src/client/simulator/dotnet/simulated-device/simulated-device/Program.cs
@@ -7,7 +7,7 @@ namespace simulated_device
         // The device connection string to authenticate the device with your IoT hub.
         // Using the Azure CLI:
         // az iot hub device-identity show-connection-string --hub-name {YourIoTHubName} --device-id MyDotnetDevice --output table
-        private readonly static string connectionString = "{Your device connection string here}";
+        private readonly static string connectionString = "HostName=bobjacfoodsafety.azure-devices.net;DeviceId=MyDotnetDevice;SharedAccessKey=YJRbHoN4xE12tz7cpNZ3m7FF10LKlGUiVSvCEFA8t9Y=";
 
         static void Main(string[] args)
         {
