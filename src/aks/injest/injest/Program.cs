6b7587ea312ea6c8d5d4ba9ec55309a147d17083
Bob Jacobs <bob.jacobs@outlook.com>
2020-01-02 14:25:08 -0500

    updating injest service

--- src/aks/injest/injest/Program.cs
+++ src/aks/injest/injest/Program.cs
@@ -4,16 +4,86 @@ namespace injest
 {
     class Program
     {
-        static void Main(string[] args)
+        // Event Hub-compatible endpoint
+        // az iot hub show --query properties.eventHubEndpoints.events.endpoint --name {your IoT Hub name}
+        private readonly static string s_eventHubsCompatibleEndpoint = "{your Event Hubs compatible endpoint}";
+
+        // Event Hub-compatible name
+        // az iot hub show --query properties.eventHubEndpoints.events.path --name {your IoT Hub name}
+        private readonly static string s_eventHubsCompatiblePath = "{your Event Hubs compatible name}";
+
+        // az iot hub policy show --name service --query primaryKey --hub-name {your IoT Hub name}
+        private readonly static string s_iotHubSasKey = "{your service primary key}";
+        private readonly static string s_iotHubSasKeyName = "service";
+        private static EventHubClient s_eventHubClient;
+                // Asynchronously create a PartitionReceiver for a partition and then start
+        // reading any messages sent from the simulated client.
+        private static async Task ReceiveMessagesFromDeviceAsync(string partition, CancellationToken ct)
         {
-            var counter = 0;
-            var max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
-            while (max == -1 || counter < max)
+            // Create the receiver using the default consumer group.
+            // For the purposes of this sample, read only messages sent since
+            // the time the receiver is created. Typically, you don't want to skip any messages.
+            var eventHubReceiver = s_eventHubClient.CreateReceiver("$Default", partition, EventPosition.FromEnqueuedTime(DateTime.Now));
+            Console.WriteLine("Create receiver on partition: " + partition);
+            while (true)
             {
-                counter++;
-                Console.WriteLine($"Counter: {counter}");
-                System.Threading.Tasks.Task.Delay(1000).Wait();
+                if (ct.IsCancellationRequested) break;
+                Console.WriteLine("Listening for messages on: " + partition);
+                // Check for EventData - this methods times out if there is nothing to retrieve.
+                var events = await eventHubReceiver.ReceiveAsync(100);
+
+                // If there is data in the batch, process it.
+                if (events == null) continue;
+
+                foreach(EventData eventData in events)
+                {
+                  string data = Encoding.UTF8.GetString(eventData.Body.Array);
+                  Console.WriteLine("Message received on partition {0}:", partition);
+                  Console.WriteLine("  {0}:", data);
+                  Console.WriteLine("Application properties (set by device):");
+                  foreach (var prop in eventData.Properties)
+                  {
+                    Console.WriteLine("  {0}: {1}", prop.Key, prop.Value);
+                  }
+                  Console.WriteLine("System properties (set by IoT Hub):");
+                  foreach (var prop in eventData.SystemProperties)
+                  {
+                    Console.WriteLine("  {0}: {1}", prop.Key, prop.Value);
+                  }
+                }
             }
+        }
+
+        private static async Task Main(string[] args)
+        {
+            Console.WriteLine("IoT Hub Quickstarts - Read device to cloud messages. Ctrl-C to exit.\n");
+
+            // Create an EventHubClient instance to connect to the
+            // IoT Hub Event Hubs-compatible endpoint.
+            var connectionString = new EventHubsConnectionStringBuilder(new Uri(s_eventHubsCompatibleEndpoint), s_eventHubsCompatiblePath, s_iotHubSasKeyName, s_iotHubSasKey);
+            s_eventHubClient = EventHubClient.CreateFromConnectionString(connectionString.ToString());
+
+            // Create a PartitionReciever for each partition on the hub.
+            var runtimeInfo = await s_eventHubClient.GetRuntimeInformationAsync();
+            var d2cPartitions = runtimeInfo.PartitionIds;
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            Console.CancelKeyPress += (s, e) =>
+            {
+                e.Cancel = true;
+                cts.Cancel();
+                Console.WriteLine("Exiting...");
+            };
+
+            var tasks = new List<Task>();
+            foreach (string partition in d2cPartitions)
+            {
+                tasks.Add(ReceiveMessagesFromDeviceAsync(partition, cts.Token));
+            }
+
+            // Wait for all the PartitionReceivers to finsih.
+            Task.WaitAll(tasks.ToArray());
         }
     }
 }
