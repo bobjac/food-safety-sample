a6cbac3ae32b439897bab1724d647f7867325177
Bob Jacobs <bob.jacobs@outlook.com>
2020-01-02 12:58:59 -0500

    adding Dockerfile

--- src/aks/injest/injest/Program.cs
+++ src/aks/injest/injest/Program.cs
@@ -6,7 +6,14 @@ namespace injest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var counter = 0;
+            var max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
+            while (max == -1 || counter < max)
+            {
+                counter++;
+                Console.WriteLine($"Counter: {counter}");
+                System.Threading.Tasks.Task.Delay(1000).Wait();
+            }
         }
     }
 }
