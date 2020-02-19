b10b0976c0ef60ad7329138b1ed93db4360c726f
Bob Jacobs <bob.jacobs@outlook.com>
2020-02-18 20:16:23 -0500

    updating the solidity smart contract

--- src/blockchain/smart-contracts/contracts/FoodSafety.sol
+++ src/blockchain/smart-contracts/contracts/FoodSafety.sol
@@ -9,6 +9,7 @@ contract FoodSafety
         string timestamp;
     }
 
+    uint public numRecords;
     Record[] public records;
 
     address private owner;
@@ -19,13 +20,13 @@ contract FoodSafety
     //List of properties
     StateType public  State;
 
-
     event StateChanged(string stateData);
 
     // constructor function
     constructor() public
     {
         owner = msg.sender;
+        numRecords = 0;
 
         emit StateChanged('Request');
     }
@@ -37,6 +38,48 @@ contract FoodSafety
         r.temperature = temperature;
         r.humidity = humidity;
         r.timestamp = timestamp;
-        records.push(r);
+        
+        if (isValid(r)) {
+            records.push(r);
+            numRecords++;
+        }
+    }
+    function isValid(Record memory r) private returns (bool) {
+        bool isValid = true;
+        int256 minTemp = -50;
+        int256 maxTemp = 50;
+        int minHumidity = -50;
+        int maxHumidity = 50;
+        
+        if (r.temperature < minTemp || r.temperature > maxTemp) {
+            State = StateType.NonCompliant;
+            emit StateChanged("HORRIBLE!!!!!!");
+            isValid = false;
+        }
+        
+        if (r.humidity < minHumidity || r.humidity > maxHumidity) {
+            State = StateType.NonCompliant;
+            emit StateChanged("HORRIBLE!!!!!!");
+            isValid = false;
+        }
+        
+        return isValid;
+    }
+    
+    function GetRecordCount() public returns (uint) {
+        return numRecords;
+    }
+    
+    function GetRecordByIndex(uint index) public returns (string memory, int256, int256, string memory) {
+        if (index > numRecords) {
+            // throw an error
+        }
+        
+        Record memory r = records[index];
+        return (r.sensorId, r.temperature, r.humidity, r.timestamp);
+    }
+    
+    function GetContractState() public returns (StateType) {
+        return State;
     }
 }
